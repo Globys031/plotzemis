80a441a7db7c269c4365fabfdfe548bc2453dec9
Globys031 <universitystuff@admins-MBP-2.lan>
2022-09-22 18:16:49 +0300

    chore: removed article

--- go/db/models/userPost.go
+++ go/db/models/userPost.go
@@ -11,9 +11,9 @@ type UserPost struct {
 	UserId      int64          `json:"userId"`
 	City        string         `json:"city" validate:"required,max=20,min=6"`
 	District    string         `json:"district" validate:"required,max=40,min=8"`
-	AreaSize    int64          `json:"areasize" validate:"required,gt=1,lt=999999"` // square meters
+	AreaSize    int64          `json:"areaSize" validate:"required,gt=1,lt=999999"` // square meters
 	Price       int64          `json:"price" validate:"required,gt=1,lt=999999999"` // euros
-	RoomCount   int64          `json:"roomcount" validate:"required,gt=1,lt=999"`
+	RoomCount   int64          `json:"roomCount" validate:"required,gt=1,lt=999"`
 	Date        datatypes.Date `json:"date"`
 	Description string         `json:"description" validate:"max=2000"`
 }
